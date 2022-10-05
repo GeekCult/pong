b39a2adbda21ce3a4946fd1264fb6af2b820ab2a
Carlos Garcia <publicidade.exe@gmail.com>
2022-10-05 12:37:17 -0300

    Visual Updates

--- src/scenes/PongScene.ts
+++ src/scenes/PongScene.ts
@@ -1,6 +1,6 @@
 import Phaser from 'phaser'
 import { pongManager } from "../game/PongManager";
-import * as Game from '../consts/Game'
+import * as C from '../consts/Game'
 
 export default class PongScene extends Phaser.Scene
 {
@@ -31,101 +31,49 @@ export default class PongScene extends Phaser.Scene
         });
         
         //Set Physics to some elements
-        this.stripeUp = pongManager.addPhysics({ game: this, y: 400, x: 0, item: 'border_h'  });
-        this.stripeDown = pongManager.addPhysics({ game: this, y: 400, x: 600, item: 'border_h'  });
-        this.borderLeft = pongManager.addPhysics({ game: this, y: 0, x: 300, item: 'border_v'  });
-        this.borderRight = pongManager.addPhysics({ game: this, y: 800, x: 300, item: 'border_v'  }); 
+        this.stripeUp = pongManager.addPhysics({ game: this, y: 400, x: 0, item: 'border_h' });
+        this.stripeDown = pongManager.addPhysics({ game: this, y: 400, x: 600, item: 'border_h' });
+        this.borderLeft = pongManager.addPhysics({ game: this, y: 0, x: 300, item: 'border_v' });
+        this.borderRight = pongManager.addPhysics({ game: this, y: 800, x: 300, item: 'border_v' }); 
         this.playerLeft = pongManager.addPhysics({ game: this, y: 30, x: 300, item: 'bar'  });
-        this.playerRight = pongManager.addPhysics({ game: this, y: 770, x: 300, item: 'bar'  });
-        this.ball = pongManager.addPhysics({ game: this, y: 400, x: 300, item: 'ball'  });
+        this.playerRight = pongManager.addPhysics({ game: this, y: 770, x: 300, item: 'bar' });
+        this.ball = pongManager.addPhysics({ game: this, y: 400, x: 300, item: 'ball' });
         
         //Set particles to ball just for fun
         const setParticles = pongManager.addParticle(this);
         
         //Add Colliders for elements
-        pongManager.addCollider({game: this, item1: this.ball, item2: this.playerLeft, action: 'right'});
-        pongManager.addCollider({game: this, item1: this.ball, item2: this.playerRight, action: 'left'});
-        pongManager.addCollider({game: this, item1: this.ball, item2: this.stripeUp, action: 'down'});
-        pongManager.addCollider({game: this, item1: this.ball, item2: this.stripeDown, action: 'up'});
+        pongManager.addCollider({ game: this, item1: this.ball, item2: this.playerLeft, action: C.RIGHT });
+        pongManager.addCollider({ game: this, item1: this.ball, item2: this.playerRight, action: C.LEFT });
+        pongManager.addCollider({ game: this, item1: this.ball, item2: this.stripeUp, action: C.DOWN });
+        pongManager.addCollider({ game: this, item1: this.ball, item2: this.stripeDown, action: C.UP });
         
-        pongManager.addCollider({game: this, item1: this.ball, item2: this.borderLeft, action: 'player1'});
-        pongManager.addCollider({game: this, item1: this.ball, item2: this.borderRight, action: 'player2'});
-        /*
-        this.physics.add.collider(
-            this.ball,
-            this.playerLeft,
-            () => {           
-                this.sideH = 'right';
-            }
-        ); 
-
-        this.physics.add.collider(
-            this.ball,
-            this.playerRight,
-            () => {           
-                this.sideH = 'left';
-            }
-        ); 
-
-        this.physics.add.collider(
-            this.ball,
-            this.stripeUp,
-            () => {           
-                this.sideV = 'down';
-            }
-        );
-
-        this.physics.add.collider(
-            this.ball,
-            this.stripeDown,
-            () => {           
-                this.sideV = 'up';
-            }
-        ); 
-        
-        this.physics.add.collider(
-            this.ball,
-            this.borderLeft,
-            () => { 
-                this.scene.pause();
-                this.goal = 'player1'; 
-                pongManager.scoreBoard(this);
-            }
-        );
-        
-        this.physics.add.collider(
-            this.ball,
-            this.borderRight,
-            () => { 
-                this.scene.pause();
-                this.goal = 'player2'; 
-                pongManager.scoreBoard(this);
-            }
-        );*/
-        
+        pongManager.addCollider({ game: this, item1: this.ball, item2: this.borderRight, action: C.PLAYER1 });
+        pongManager.addCollider({ game: this, item1: this.ball, item2: this.borderLeft, action: C.PLAYER2 });
+                
     }
     
     update() : void {
         
-        if(this.sideV == 'down'){  this.ball.y += this.sideM; }
-        if(this.sideV == 'up'){    this.ball.y -= this.sideM; }
-        if(this.sideH == 'right'){ this.ball.x += this.sideN; } 
-        if(this.sideH == 'left'){  this.ball.x -= this.sideN; }       
+        if(this.sideV == C.DOWN){  this.ball.y += this.sideM; }
+        if(this.sideV == C.UP){    this.ball.y -= this.sideM; }
+        if(this.sideH == C.RIGHT){ this.ball.x += this.sideN; } 
+        if(this.sideH == C.LEFT){  this.ball.x -= this.sideN; }       
          
         if (this.keys.w.isDown){
-            if(this.playerLeft.y > Game.PLAYER_TOP){ this.playerLeft.y -= Game.PLAYER_SPEED; }
+            if(this.playerLeft.y > C.PLAYER_TOP){ this.playerLeft.y -= C.PLAYER_SPEED; }
         }
         
         if (this.keys.s.isDown){  
-            if(this.playerLeft.y < Game.PLAYER_BOTTOM){ this.playerLeft.y += Game.PLAYER_SPEED; }
+            if(this.playerLeft.y < C.PLAYER_BOTTOM){ this.playerLeft.y += C.PLAYER_SPEED; }
         }
         
         if (this.keys.up.isDown){  
-            if(this.playerRight.y > Game.PLAYER_TOP){ this.playerRight.y -= Game.PLAYER_SPEED; }
+            if(this.playerRight.y > C.PLAYER_TOP){ this.playerRight.y -= C.PLAYER_SPEED; }
         }
         
         if (this.keys.down.isDown){   
-            if(this.playerRight.y < Game.PLAYER_BOTTOM){ this.playerRight.y += Game.PLAYER_SPEED; }
+            if(this.playerRight.y < C.PLAYER_BOTTOM){ this.playerRight.y += C.PLAYER_SPEED; }
         }
         
         //Reset game
